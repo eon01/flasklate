fed30310a241676a862b984a64c9f4398837da69
root <amri.aymen@gmail.com>
2017-03-03 16:05:15 +0100

    add main and hello world functions

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-import logging, traceback, ConfigParser
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging, traceback, ConfigParser, os
+from flask import Flask
+app = Flask(__name__)
+
 
 # start configuration parser
 parser = ConfigParser.ConfigParser()
@@ -24,3 +30,15 @@ handler.setFormatter(formatter)
 
 # add the handlers to the logger
 logger.addHandler(handler)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
